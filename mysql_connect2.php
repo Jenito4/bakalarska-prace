c9544913f33e0adfadeac9db5f2b8d1dd433a580
Jenito4 <102227021+Jenito4@users.noreply.github.com>
2022-03-23 12:44:13 +0100

    Update mysql_connect2.php

--- mysql_connect2.php
+++ mysql_connect2.php
@@ -2,10 +2,10 @@
 // This file contains the database access information. This file also establishes a connection to MySQL and selects the database.
 
 // Set the database access information as constants.
-DEFINE ('DB_USER', 'agenturalegiscz');
-DEFINE ('DB_PASSWORD', 'Jm_16.10.98');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'legis');
+DEFINE ('DB_USER', '');
+DEFINE ('DB_PASSWORD', '');
+DEFINE ('DB_HOST', '');
+DEFINE ('DB_NAME', '');
 
 $dbc = @mysql_connect (DB_HOST, DB_USER, DB_PASSWORD) OR die ('Nelze se připojit k MySQL: ' . mysql_error() );
 mysql_select_db (DB_NAME) OR die ('Nelze vybrat databázi: ' . mysql_error() );
